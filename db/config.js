deae4f1cc7ce84eaf781901771f211082452ce20
wuhe <15156280727@163.com>
2020-09-15 13:48:37 +0800

    更改数据库请求地址

--- db/config.js
+++ db/config.js
@@ -1,17 +1,8 @@
 let dbOption
 
-// dbOption = {
-//   connectionLimit: 10,
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'root',
-//   port: '3306',
-//   database: 'web-blog'
-// }
-
 dbOption = {
   connectionLimit: 10,
-  host: '192.168.56.143',
+  host: '192.168.6.100',
   user: 'root',
   password: 'zr261823811111/',
   port: '3306',
